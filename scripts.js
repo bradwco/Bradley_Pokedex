470be107e3a63d7f1f0bec4989fbdba522535c5e
Bradley Co <bradco626@gmail.com>
2026-04-21 12:05:08 -0700

    All pokemon viewable from csv

--- scripts.js
+++ scripts.js
@@ -1,97 +1,76 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
+/* Data Sources:
+Sprites - Repository: https://github.com/PokeAPI/sprites
+  Pattern: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png
+Audios(?) - Pokemon Showdown: https://github.com/smogon/pokemon-showdown-client, https://github.com/PokeAPI/cries
+  Pattern: https://play.pokemonshowdown.com/audio/cries/{name}.mp3
+Stats - Repository: https://github.com/lgreski/pokemonData, https://pokemondb.net  
+*/
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+// Stats Importing 
+// "ID","Name","Form","Type1","Type2","Total","HP","Attack","Defense","Sp. Atk","Sp. Def","Speed","Generation"
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+let pokemonData = [];
+fetch("data/Pokemon_Stats.csv")
+  .then(response => response.text())
+  .then(text => {
+    const rows = text.split("\n")
+    const headers = rows[0].split(",")
 
-// This function adds cards the page to display the data in the array
-function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
+    const pokemon = rows.slice(1)
+      .map(row => row.split(","))
+      .filter(cols => Number(cols[0]) <= 151 && cols[0] != "")
+      .map(cols => {
+        const clean = cols.map(c => c.replace(/"/g, "").trim());
+        
+        return {
+          id: Number(clean[0]),
+          name: clean[1],
+          type1: clean[3],
+          type2: clean[4] || null,
+          hp: Number(clean[5]),
+          attack: Number(clean[6]),
+          defense: Number(clean[7]),
+          spAtk: Number(clean[8]),
+          spDef: Number(clean[9]),
+          speed: Number(clean[10]),
+          generation: Number(clean[11])
+        };
+      });
+      pokemonData = pokemon;
+      init();
+  }); 
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
+  // Sprite Getter
+  function getSprite(id){
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   }
-}
 
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
+  // Sound Getter
+  function getSound(name){
+    return `https://play.pokemonshowdown.com/audio/cries/${name}.mp3`
+  }
 
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  // Render Grid of Pokemon
+  function renderGrid(data){
+    const grid = document.getElementById("pokemonGrid");
+    grid.innerHTML = "";
 
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
+    data.forEach(p => {
+      const card = document.createElement("div")
+      card.classList.add("poke-card")
+      card.innerHTML = `
+        <div class="poke-num">#${String(p.id).padStart(3, "0")}</div>
+        <img class="poke-sprite" src="${getSprite(p.id)}" alt="${p.name}">
+        <div class="poke-name">${p.name}</div>
+        <div class="poke-types-row">
+          <span class="poke-type-pip">${p.type1}</span>
+          ${p.type2 ? `<span class="poke-type-pip">${p.type2}</span>` : ""}
+        </div>
+      `;
+      grid.appendChild(card);
+    });
+  }
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
-}
-
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
-  );
-}
-
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-}
+  function init(){
+    renderGrid(pokemonData)
+  }
